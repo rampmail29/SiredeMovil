b58b83e3b3383b8652b19815d8e6f38fa23dde7d
rampmail29 <113305123+rampmail29@users.noreply.github.com>
2025-10-11 17:14:59 -0500

    Update InformeEstudiante

--- components/InformeEstudiante.js
+++ components/InformeEstudiante.js
@@ -32,7 +32,7 @@ const InformeEstudiante = () => {
         }))
       );
 
-      console.log("🚀 ~ buscarEstudiantes ~ data:", data[0]);
+      console.log("🚀 ~ buscarEstudiantes ~ data_:___:", data[0]);
     } catch (error) {
       showMessage({
         message: "Error",
